d657f74dec6c3cc9986f872ce436bd3fdf886642
AxelDG <104107385+AxelDG@users.noreply.github.com>
2022-04-22 19:53:22 -0300

    Initial commit

---
+++ src/ejercicio.ts
@@ -0,0 +1,37 @@
+console.log("Como hacer una torta");
+console.log("Paso 1: Conceptos básicos para hornear un pastel");
+console.log(
+  "Cuando los niños se interesan en ayudar a sus padres en la cocina, una de las primeras cosas que quieren hacer es ayudar a hornear a pastel. Es una buena opción porque hornear pasteles es a menudo un proceso fácil y directo y muchas recetas siguen los mismos pasos básicos en todo momento. Conocer los fundamentos le permite acercarse a hacer un pastel desde cero con gran confianza. Para este paso por-paso ejemplo, un pastel de chocolate está en el menú."
+);
+console.log("Paso 2: Engrasar y precalentar");
+console.log(
+  "Antes de comenzar a hornear grasa y harina las cacerolas que está utilizando. Pueden ser cacerolas redondas de 8 pulgadas rondas de 9 pulgadas, una bandeja rectangular de 13 x 9, o incluso una bandeja Bundt. Luego, precaliente el horno a la temperatura como se indica en la receta para que esté listo para hornear cuando usted lo esté."
+);
+console.log("Paso 3: Prepare sus ingredientes");
+console.log(
+  "Es importante tener todos los ingredientes preparados antes de hacer la masa. Muchas recetas de pasteles requieren chocolate derretido, así que debes hacerlo antes de comenzar a mezclar la masa. Para derretir chocolate , puede usar un microondas o una caldera doble en la estufa, el método tradicional. La preparación de otros ingredientes podría incluir picar nueces o rallar ralladura de limón o naranja."
+);
+console.log("Paso 4: Batir la mezcla seca");
+console.log(
+  "Es importante medir correctamente y luego mezclar completamente los ingredientes secos para que el bizcocho tenga una subida uniforme y adecuada. Use un batidor de varillas para combinar la harina con la agentes leudantes para obtener los mejores resultados."
+);
+console.log("Paso 5: Crema tu mantequilla y azúcar");
+console.log(
+  "Lleve la mantequilla a temperatura ambiente antes de comenzar a hornear. Cuando haya alcanzado la temperatura adecuada, bátela hasta que esté cremoso . Agregue lentamente el azúcar mientras bate la mantequilla si usa una batidora de pie. Si usa una batidor eléctrico o mezclando a mano, agregue un poco de azúcar a la vez y luego mezcle hasta que haya incorporado la cantidad completa."
+);
+console.log("Paso 6: Agregue los huevos");
+console.log(
+  "Agregue los huevos a la mezcla de mantequilla y azúcar, uno a la vez, y mezcle bien hasta que se mezclen."
+);
+console.log("Paso 7: Es hora de combinar");
+console.log(
+  "Para a pastel de chocolate, esto es generalmente cuando el chocolate, la harina y la leche se agregan a la mezcla de mantequilla y azúcar. Primero, comience con el chocolate derretido. Asegúrese de incorporarlo completamente con la mantequilla, el azúcar y los huevos combinados. Luego, agregue alternativamente los ingredientes secos y la leche a sus ingredientes húmedos. Mezcle bien hasta que esté suave y uniforme. Algunas recetas sugieren que espere hasta el final para agregar leche; siga su receta específica para obtener mejores resultados."
+);
+console.log("Paso 8: Vierta su masa en su sartén");
+console.log(
+  "Si está haciendo un pastel en capas, divida con cuidado la masa entre dos moldes para pasteles redondos. Si está usando un molde rectangular o Bundt, saque toda su masa en el molde preparado de su elección. Golpee suavemente los moldes en el mostrador paraemparejar la parte superior y eliminar las burbujas de aire. Si es necesario, utilice una espátula para suavizar la parte superior antes de hornear."
+);
+console.log("Paso 9: Hornee y enfríe sus pasteles");
+console.log(
+  "Hornea los pasteles según el tiempo y la temperatura en las instrucciones de su receta. Retire del horno y enfríe en una rejilla durante 10 minutos o como se indica en su receta. Retire los pasteles de los moldes y déjelos enfriar completamente sobre rejillas de alambre antes de agregarlos glaseado. Asegúrese de que la capa superior no esté al revés en la rejilla; de lo contrario, habrá líneas de cuadrícula en la parte superior del pastel."
+);
